eed4f968542970d06f60ce21c83e3f84b7864e19
Eric James Orate <eorate@ad.acquireap>
2018-05-31 15:31:52 +0800

    My Portfolio

---
+++ pages/home.php
@@ -0,0 +1,5 @@
+<?php include 'navpane.php';?>
+
+
+
+<?php include 'footer.php';?>
---
+++ pages/navpane.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Eric's Portfolio</title>
+    
+    <!-- LINKS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navpane.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="../images/roboarm.png">
+    <!-- END LINKS -->
+
+</head>
+<body>
+
+<div class="navpane col-s-3">
+    <div class="navcontent">
+        <div class="ribbon">   
+            <center><img src="../images/eric.png" class="photo"></center>
+            <div class="name">
+                ERIC JAMES M. ORATE
+            </div>
+            <div class="course">
+                B.S. COMPUTER SCIENCE
+            </div>
+
+            <div class="navmenu">
+                <ul class="menus">
+                    <a href=""><li class="active">HOME</li></a>
+                    <a href=""><li>ABOUT ME</li></a>
+                    <a href=""><li>WEB DESIGN PORTFOLIO</li></a>
+                    <a href=""><li>PROJECT PORTFOLIO</li></a>
+                </ul>
+            </div>       
+            <center>
+                <div class="social">
+                    <a target="_blank" href="https://www.facebook.com/ericjames.orate"><i class="fa fa-facebook" style="font-size:2vw"></i></a>
+                    <a target="_blank" href="https://plus.google.com/u/0/101864908794714657228"><i class="fa fa-google-plus" style="font-size:2vw"></i></a>
+                    <a target="_blank" href="https://www.instagram.com/elikdyems"><i class="fa fa-instagram" style="font-size:2vw"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/eric-james-orate-bb5256121"><i class="fa fa-linkedin" style="font-size:2vw"></i></a>
+                </div>
+            </center>
+            </div>
+        </div>  
+    </div>
+</div>
